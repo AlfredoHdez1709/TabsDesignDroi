4be582fdb4b0ac0f4897af68806a392baa64b146
Alfredo Hernández <alfredo_hernandez_rguez@outlook.com>
2017-02-14 10:15:36 -0600

    Tabs

---
+++ app/src/main/java/com/ahrsoft/movil/tabdroi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ahrsoft.movil.tabdroi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ahrsoft.geekahr.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
